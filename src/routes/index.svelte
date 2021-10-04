83abd27bb884648bee7e62464a819fa94b9d081b
Emmanuel Raviart <emmanuel@raviart.com>
2021-10-04 20:51:29 +0200

    Use DaisyUI.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,47 @@
-<h1>Tax-Benefit Simulator</h1>
-<p>Select the tax and benefit system you want to simulate:</p>
-<ul class="list-disc list-inside">
-  <li><a class="link" href="https://fr.tax-benefit.org">France</a></li>
-  <!-- <li>
-    <a class="link" href="https://sn.tax-benefit.org"
-      >Senegal <small class="italic">(in progress)</small></a
-    >
-  </li> -->
-  <li>
-    <a class="link" href="https://tn.tax-benefit.org"
-      >Tunisia <small class="italic">(in progress)</small></a
-    >
-  </li>
-  <li>
-    <a class="link" href="https://uk.tax-benefit.org"
-      >United Kingdom <small class="italic">(in progress)</small></a
-    >
-  </li>
-  <li>
-    <a class="link" href="https://us.tax-benefit.org"
-      >USA <small class="italic">(in progress)</small></a
-    >
-  </li>
-</ul>
+<div class="navbar bg-neutral text-neutral-content">
+  <div class="flex-1">
+    <span class="font-bold text-lg">Tax and Benefit Simulators</span>
+  </div>
+</div>
+
+<div class="hero bg-base-200">
+  <div class="hero-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl">
+        Simulate the tax and benefit system of your country
+      </h1>
+      <p>Select your tax and benefit system:</p>
+      <ul class="list-disc list-inside">
+        <li>
+          <a class="link link-primary" href="https://fr.tax-benefit.org"
+            >France <div class="badge badge-primary badge-sm">alpha</div></a
+          >
+        </li>
+        <!-- <li>
+          <a class="link" href="https://sn.tax-benefit.org"
+            >Senegal <div class="badge badge-sm badge-warning">in progress</div></a
+          >
+        </li> -->
+        <li>
+          <a class="link" href="https://tn.tax-benefit.org"
+            >Tunisia <div class="badge badge-sm badge-warning">
+              in progress
+            </div></a
+          >
+        </li>
+        <li>
+          <a class="link" href="https://uk.tax-benefit.org"
+            >United Kingdom <div class="badge badge-sm badge-warning">
+              in progress
+            </div></a
+          >
+        </li>
+        <li>
+          <a class="link" href="https://us.tax-benefit.org"
+            >USA <div class="badge badge-sm badge-warning">in progress</div></a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
